a76b44b2719b24cc17ce77dc678a6198f483b811
mcBontempi <mcbontempi@gmail.com>
2014-11-08 07:08:42 +0000

    initialCommit

---
+++ Jenkins/Jenkins/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Jenkins
+//
+//  Created by Daren David Taylor on 08/11/2014.
+//  Copyright (c) 2014 DDT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Jenkins/Jenkins/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Jenkins
+//
+//  Created by Daren David Taylor on 08/11/2014.
+//  Copyright (c) 2014 DDT. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
